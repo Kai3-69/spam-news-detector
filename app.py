9cd399ea50dafc65a42cad3860b6a60c3fead713
Kai3-69 <asgharifarima1384@gmail.com>
2026-03-14 21:57:51 +0530

    Updated app.py for deployment

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import joblib
+import os
 
 app = Flask(__name__)
 
@@ -25,5 +26,7 @@ def predict():
 
     return render_template("index.html", prediction_text=result)
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
